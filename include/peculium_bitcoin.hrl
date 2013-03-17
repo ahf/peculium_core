8a382774e4bca36792ee8261552c85dfcc2d3ee5
Alexander Færøy <ahf@0x90.dk>
2013-03-17 14:24:28 +0100

    Add type information for bitcoin units

---
+++ include/peculium_bitcoin.hrl
@@ -0,0 +1,3 @@
+-type bitcoin_unit_atom() :: megabitcoin | kilobitcoin | hectobitcoin | decabitcoin
+                           | bitcoin | decibitcoin | centibitcoin | millibitcoin
+                           | microbitcoin | satoshi.
