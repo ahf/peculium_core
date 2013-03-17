2c96e4f2a5751e5b089bab69dc506e055010dccf
Alexander Færøy <ahf@0x90.dk>
2013-03-17 14:29:18 +0100

    Add protocol types

---
+++ include/peculium_bitcoin_protocol.hrl
@@ -0,0 +1,32 @@
+-type uint8_t()  :: 0..255.
+-type uint16_t() :: 0..65535.
+-type uint32_t() :: 0..4294967295.
+-type uint64_t() :: 0..18446744073709551615.
+
+%% Erlang doesn't support -xxx..yyy ranges :-(
+-type int8_t()  :: integer().
+-type int16_t() :: integer().
+-type int32_t() :: integer().
+-type int64_t() :: integer().
+
+-type bitcoin_network_atom() :: mainnet | testnet | testnet3.
+
+-type bitcoin_command_atom() :: addr | alert | block | checkorder
+                              | getaddr | getblocks | getdata | getheaders
+                              | headers | inv | ping | submitorder
+                              | reply | tx | verack | version.
+
+-type bitcoin_inv_atom() :: error | tx | block.
+-type bitcoin_inv_integer() :: 0 | 1 | 2.
+
+-type bitcoin_checksum() :: <<_:32>>.
+
+-record(bitcoin_message_header, {
+    network :: bitcoin_network_atom(),
+    command :: bitcoin_command_atom(),
+    length :: uint32_t(),
+    checksum :: bitcoin_checksum()
+}).
+
+-record(bitcoin_message, {
+}).
