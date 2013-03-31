ca55bc067fd48a05deadb974db07d14eba509593
Alexander Færøy <ahf@0x90.dk>
2013-03-31 14:43:44 +0200

    Add reverse/1, hex2bin/1 and bin2hex/1

--- src/peculium_utilities.erl
+++ src/peculium_utilities.erl
@@ -1,10 +1,18 @@
 -module(peculium_utilities).
 
 -export([strip/2, timestamp/0]).
+-export([hex2bin/1, bin2hex/1]).
+-export([reverse/1]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
+
+-spec reverse(binary()) -> binary().
+reverse(Binary) when is_binary(Binary) ->
+    Size = byte_size(Binary) * 8,
+    <<X:Size/integer-little>> = Binary,
+    <<X:Size/integer-big>>.
 
 -spec strip(binary(), binary()) -> binary().
 strip(Subject, Pattern) ->
@@ -15,6 +23,18 @@ strip(Subject, Pattern) ->
 timestamp() ->
     {MegaSeconds, Seconds, _MicroSeconds} = now(),
     MegaSeconds * 1000000 + Seconds.
+
+-spec hex2bin(string()) -> binary().
+hex2bin([A, B | Rest]) ->
+    <<(list_to_integer([A, B], 16)), (hex2bin(Rest))/binary>>;
+hex2bin([A]) ->
+    <<(list_to_integer([A], 16))>>;
+hex2bin([]) ->
+    <<>>.
+
+-spec bin2hex(binary()) -> string().
+bin2hex(Bin) when is_binary(Bin) ->
+    lists:flatten([integer_to_list(X, 16) || <<X:4/integer>> <= Bin]).
 
 -ifdef(TEST).
 
