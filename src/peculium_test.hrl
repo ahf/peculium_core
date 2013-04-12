b1ccdd8b4eaaf1154f4305dbd9ce27f6618b7c76
Alexander Færøy <ahf@0x90.dk>
2013-04-12 19:54:22 +0200

    Add test header

---
+++ src/peculium_test.hrl
@@ -0,0 +1,12 @@
+%% @author Alexander Færøy <ahf@0x90.dk>
+%% @copyright 2013 Alexander Færøy
+%% @doc Peculium Testing Utilities.
+
+-ifdef(TEST).
+
+-include_lib("triq/include/triq.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-spec test() -> any().
+
+-endif.
