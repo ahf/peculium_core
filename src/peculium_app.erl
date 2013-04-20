f2aa9cee6e104f448ce50710d5aa41e31c182719
Alexander Færøy <ahf@0x90.dk>
2013-04-20 18:33:04 +0200

    Ensure various directories are available when we are started

--- src/peculium_app.erl
+++ src/peculium_app.erl
@@ -10,12 +10,22 @@ start() ->
 
 -spec start(normal | {takeover, node()} | {failover, node()}, term()) -> {ok, pid()}.
 start(_, _) ->
+    %% Start peer listeners.
     {ok, _} = ranch:start_listener(peculium_listener, 100,
         ranch_tcp, [{port, 5555}],
         peculium_peer, []),
 
+    %% Set debug log level.
     lager:set_loglevel(lager_console_backend, debug),
-    peculium_sup:start_link().
+
+    %% Start the supervisor.
+    Result = peculium_sup:start_link(),
+
+    %% Ensure that the dot directories exists.
+    filelib:ensure_dir(peculium_config:dotdir() ++ "/"),
+    filelib:ensure_dir(peculium_config:block_chain_dir() ++ "/"),
+
+    Result.
 
 -spec stop([]) -> ok.
 stop(_State) ->
