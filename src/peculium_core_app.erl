a7e651e08a3123c0750d3632fd1d5585e6929f25
Alexander Færøy <ahf@0x90.dk>
2013-08-01 16:47:14 +0200

    Don't set logger options in start()

--- src/peculium_core_app.erl
+++ src/peculium_core_app.erl
@@ -39,14 +39,6 @@ start() ->
 
 -spec start(normal | {takeover, node()} | {failover, node()}, term()) -> {ok, pid()}.
 start(_, _) ->
-    %% Start peer listeners.
-    %% {ok, _} = ranch:start_listener(peculium_core_listener, 100,
-    %%    ranch_tcp, [{port, 5555}],
-    %%    peculium_core_peer, []),
-
-    %% Set debug log level.
-    lager:set_loglevel(lager_console_backend, debug),
-
     %% Start the supervisor.
     peculium_core_sup:start_link().
 
